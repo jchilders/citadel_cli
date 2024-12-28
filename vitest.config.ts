47da61d4d7ea5cd8ea6436af4134ff916c10449b
James Childers <james.childers@gmail.com>
2024-12-28 17:06:22 -0500

    do typechecking when running tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     setupFiles: ['./src/test/setup.ts'],
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    typecheck: {
+      enabled: true,
+      tsconfig: './tsconfig.json'
+    },
     coverage: {
       exclude: ['node_modules', 'dist', 'command_examples', 'prompts', '.cache',
         '*.js', '*.cjs', '*.ts', // exclude config files in project root
