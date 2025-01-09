6a30a59e93ba5ff4425b208cff2394dcdc6a4d77
James Childers <james.childers@gmail.com>
2025-01-09 11:01:04 -0500

    feat: Configure Citadel with customer service commands and BBS cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Citadel } from './components/Citadel';
-import { commands } from '../command_examples/basic-commands';
-// import { commands } from '../command_examples/devops-commands';
-// import { commands } from '../command_examples/customer-service-commands';
+import { CitadelConfig } from './components/Citadel/config/types';
 
-// export const config = {
+// import { commands } from '../command_examples/basic-commands';
+// import { commands } from '../command_examples/devops-commands';
+import { commands } from '../command_examples/customer-service-commands';
+
+export const config: CitadelConfig = {
+  cursorType: "bbs"
   // commandTimeoutMs: 10000, // default 10000
   // includeHelpCommand: false, // default true
   // resetStateOnHide: true, // default true
   // showCitadelKey: '.', // default '.'
   // maxHeight: '80vh', // default '80vh'
-// };
+};
 
 function App() {
   return (
@@ -18,7 +21,7 @@ function App() {
         <p className="text-gray-700">
           Press <code className="px-2 py-1 bg-gray-100 border border-gray-300 rounded">.</code>
         </p>
-        {/* <Citadel config={config} commands={commands} /> */}
+        <Citadel config={config} commands={commands} /> 
         <Citadel commands={commands} />
       </div>
     </div>
