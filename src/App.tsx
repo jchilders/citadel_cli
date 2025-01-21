57ee7360d0b12df25fbfdaf4844429adc4f95b33
James Childers <james.childers@gmail.com>
2025-01-20 20:35:13 -0500

    minor tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-800 flex items-center justify-center">
       <div className="bg-white rounded-lg shadow-lg p-6">
-        <p className="text-gray-700">
-          Press <code className="px-2 py-1 bg-gray-100 border border-gray-300 rounded">.</code>
+        <p className="text-center text-gray-700">
+          Press <code className="px-2 border border-gray-300 rounded">.</code> to<br/>activate Citadel
         </p>
         <Citadel config={config} commands={commands} />
       </div>
